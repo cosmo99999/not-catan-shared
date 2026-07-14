caf6df09b9a5179286ffb5aefc949366591b35b7
Rory Coleman <rory.cosmic@yahoo.com>
2026-07-14 18:38:30 +1000

    init

---
+++ src/model.ts
@@ -0,0 +1,373 @@
+import { get } from "node:http";
+
+const offsets: [number, number][] = [[0, -1], [1, -1], [1, 0], [0, 1], [-1, 1], [-1, 0]];
+const tilePositon: [number, number][] = [
+  [0, 0], [0, -1], [1, -1], [1, 0], [0, 1], [-1, 1], [-1, 0], [0, -2], [1, -2],
+  [2, -2], [2, -1], [2, 0], [1, 1], [0, 2], [-1, 2], [-2, 2], [-2, 1], [-2, 0], [-1, -1]
+]
+const possiblenumbers = [2, 12, 11, 11, 3, 3, 4, 4, 5, 5, 6, 6, 8, 8, 9, 9, 10, 10];
+const possilberesources = [0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4];
+const portNums = [0, 1, 2, 3, 4, 5, 5, 5, 5];
+const portSpacings = [3, 4, 3, 3, 3, 3, 3, 4, 3];
+const portVertexOrderedIds = [26, 27, 24, 25, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 51, 52, 50, 53];
+const size = 65;
+function hexCorner(q: number, r: number, i: number) {
+  const { x: cx, y: cy } = axialToPixel(q, r);
+  const angleDeg = 60 * i - 30; // pointy-top: corners at 30°, 90°, 150°...
+  const angleRad = (Math.PI / 180) * angleDeg;
+  return {
+    x: cx! + size * Math.cos(angleRad),
+    y: cy! + size * Math.sin(angleRad),
+  };
+}
+
+export function axialToPixel(q: number, r: number) {
+  return {
+    x: size * Math.sqrt(3) * (q + r / 2),
+    y: size * 1.5 * r,
+  };
+}
+function tileKey(q: number, r: number): string {
+  return `${q},${r}`;
+}
+
+function vertKey(q: number, r: number, k: number): string {
+  const [qO1, r01] = offsets[k]!;
+  const [qO2, r02] = offsets[(k + 1) % 6]!;
+  const neighbours = [
+    tileKey(q, r),
+    tileKey(q + qO1, r + r01),
+    tileKey(q + qO2, r + r02),
+  ]
+  return neighbours.sort().join("|");
+}
+
+function edgeKey(q: number, r: number, k: number): string {
+  const [qO1, r01] = offsets[k]!;
+  const neighbours = [
+    tileKey(q, r),
+    tileKey(q + qO1, r + r01),
+  ]
+  return neighbours.sort().join("|");
+}
+function getRandomInt(min: number, max: number): number {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  // The maximum is exclusive and the minimum is inclusive
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+}
+enum Resource {
+  Brick,
+  Wood,
+  Sheep,
+  Wheat,
+  Ore,
+  None,
+}
+enum StructureType {
+  Settlement,
+  City,
+  Road
+}
+enum Colour {
+  Blue,
+  Orange,
+  Red,
+  White
+}
+class Card {
+  resource: Resource;
+  constructor(resource: Resource) {
+    this.resource = resource;
+  }
+}
+class Structure {
+  id: number;
+  type: StructureType;
+  colour: Colour;
+  constructor(id: number, c: Colour, type: StructureType) {
+    this.id = id;
+    this.type = type;
+    this.colour = c;
+  }
+  toJSON() {
+    return {
+      id: this.id,
+      type: this.type,
+      colour: this.colour,
+    }
+  }
+}
+class Port {
+  id: number;
+  rate: number = -1;
+  resource: Resource = Resource.None;
+  constructor(id: number, rate: number, resource: Resource) {
+    this.id = id;
+    this.rate = rate;
+    this.resource = resource;
+  }
+  toJSON() {
+    return {
+      id: this.id,
+      rate: this.rate,
+      resource: this.resource,
+    }
+  }
+}
+class Edge {
+  id: number;
+  vertices: [Vertice, Vertice];
+  tiles: Tile[] = [];
+  structure?: Structure;
+  constructor(id: number, v1: Vertice, v2: Vertice) {
+    this.vertices = [v1, v2];
+    this.id = id;
+  }
+  toJSON() {
+    return {
+      id: this.id,
+      structure: this.structure,
+      vertexIds: this.vertices.map(v => v.id),
+      tileIds: this.tiles.map(e => e.id),
+    }
+  }
+}
+class Tile {
+  id: number;
+  r: number;
+  q: number;
+  edges: Edge[] = [];
+  vertices: Vertice[] = [];
+  resource: Resource = Resource.None;
+  value: number = -1;
+  robber: boolean = false;
+  xPos: number = -1;
+  yPos: number = -1;
+  constructor(id: number, q: number, r: number) {
+    this.id = id;
+    this.q = q;
+    this.r = r;
+
+  }
+  toJSON() {
+    return {
+      id: this.id,
+      r: this.r,
+      q: this.q,
+      vertexIds: this.vertices.map(v => v.id),
+      edgeIds: this.edges.map(e => e.id),
+      resource: this.resource,
+      value: this.value,
+      robber: this.robber,
+      xPos: this.xPos,
+      yPos: this.yPos,
+    }
+  }
+}
+class Vertice {
+  id: number;
+  edges: Edge[] = [];
+  port?: Port;
+  tiles: Tile[] = [];
+  structure?: Structure;
+  xPos: number = -1;
+  yPos: number = -1;
+  constructor(id: number) {
+    this.id = id;
+  }
+  toJSON() {
+    return {
+      id: this.id,
+      edgeIds: this.edges.map(e => e.id),
+      tileIds: this.tiles.map(t => t.id),
+      structureId: this.structure?.id,
+      portId: this.port?.id,
+      xPos: this.xPos,
+      yPos: this.yPos,
+    }
+  }
+}
+class Player {
+  id: number;
+  name?: string;
+  victoryPoints: number = 0;
+  colour: Colour;
+  structures: Structure[] = [];
+  cards: Card[] = [];
+  constructor(id: number, c: Colour) {
+    this.id = id;
+    this.colour = c;
+  }
+  toJSON() {
+    return {
+      id: this.id,
+      structureIds: this.structures.map(t => t.id),
+      colour: this.colour,
+      cards: this.cards,
+    }
+  }
+}
+export class Game {
+  tiles: Tile[] = [];
+  vertices: Vertice[] = [];
+  edges: Edge[] = [];
+  players: Player[] = [];
+  ports: Port[] = [];
+  structures: Structure[] = [];
+
+
+  constructor() {
+    this.buildBoard();
+  }
+
+  buildBoard() {
+    const tiles: Tile[] = [];
+    const vertexLookup = new Map<string, Vertice>();
+    const edgeLookup = new Map<string, Edge>();
+    let nextEId = 0;
+    let nextVId = 0;
+
+    tilePositon.forEach(([q, r], i) => {
+      const tile = new Tile(i, q, r);
+      const { x: xp, y: yp } = axialToPixel(q, r)!;
+      tile.xPos = xp;
+      tile.yPos = yp;
+      const cornerVerts: Vertice[] = [];
+
+      for (let k = 0; k < 6; k++) {
+        const key = vertKey(q, r, k)
+        let v = vertexLookup.get(key);
+        if (!v) {
+          v = new Vertice(nextVId++);
+          const { x: vxp, y: vyp } = hexCorner(q, r, (k + 5) % 6);
+          v.xPos = vxp;
+          v.yPos = vyp;
+          vertexLookup.set(key, v);
+        }
+        v.tiles.push(tile);
+        tile.vertices.push(v);
+        cornerVerts.push(v);
+      }
+      for (let k = 0; k < 6; k++) {
+        const key = edgeKey(q, r, k)
+        let e = edgeLookup.get(key);
+        if (!e) {
+          let v1 = (k + 5) % 6;
+          let v2 = k;
+          e = new Edge(nextEId++, cornerVerts[v1]!, cornerVerts[k]!);
+          edgeLookup.set(key, e);
+          e.vertices[0].edges.push(e);
+          e.vertices[1].edges.push(e);
+        }
+        e.tiles.push(tile);
+        tile.edges.push(e);
+      }
+      tiles.push(tile);
+    });
+    this.tiles = tiles;
+    this.edges = [...edgeLookup.values()];
+    this.vertices = [...vertexLookup.values()];
+
+  }
+  randomizeBoard() {
+    const numbers: number[] = [...possiblenumbers];
+    const resources: number[] = [...possilberesources];
+    const tileOffset = getRandomInt(0, 10);
+    for (let i = 0; i < this.tiles.length; i++) {
+      const t = this.tiles[(i + tileOffset) % (this.tiles.length)]!;
+      if (numbers.length == 0) {
+        t.resource = Resource.None;
+        t.value = -1;
+      } else {
+        let nIndex = getRandomInt(0, numbers.length);
+        let rIndex = getRandomInt(0, resources.length);
+
+        const n = numbers[nIndex];
+        const r = resources[rIndex];
+
+        t.resource = r!;
+        t.value = n!;
+
+        numbers.splice(nIndex, 1);
+        resources.splice(rIndex, 1);
+      }
+    };
+    let portIdCounter = 0;
+    const portsNumbers = [...portNums];
+    const verticeNums = [...portVertexOrderedIds];
+    const spacings = [...portSpacings];
+    const offset = getRandomInt(0, 10);
+    let counter = 0;
+    for (let i = 0; i < verticeNums.length;) {
+      if (portsNumbers.length == 0) break;
+      const v1 = this.vertices.find(v => v.id == verticeNums[(i + offset) % verticeNums.length])!;
+      const v2 = this.vertices.find(v => v.id == verticeNums[(i + offset + 1) % verticeNums.length])!;
+      const pIndex = getRandomInt(0, portsNumbers.length);
+      const resource = portsNumbers[pIndex]!;
+      portsNumbers.splice(pIndex, 1);
+      let value = -1;
+      if (resource !== 5) {
+        value = 2;
+      } else {
+        value = 3;
+      }
+      const p = new Port(portIdCounter++, value, resource);
+      v1.port = p;
+      v2.port = p;
+      this.ports.push(p);
+
+      const sIndex = getRandomInt(0, spacings.length);
+      const s = spacings[sIndex]!;
+      i += s;
+      spacings.splice(sIndex, 1);
+    }
+  }
+  toJSON() {
+    return {
+      Tiles: this.tiles.map(t => t.toJSON()),
+      Vertices: this.vertices.map(v => v.toJSON()),
+      Edges: this.edges.map(e => e.toJSON()),
+      Players: this.players.map(p => p.toJSON()),
+      Ports: this.ports.map(p => p.toJSON()),
+      Structures: this.structures.map(s => s.toJSON()),
+    }
+  }
+  seed() {
+    let sCounter = 0;
+    // for (let i = 0; i < 10; i++) {
+    //   let pos = getRandomInt(0, this.edges.length);
+    //   let c = getRandomInt(0, 4);
+    //   let s = new Structure(sCounter++, c, StructureType.Road);
+    //   this.edges[pos]!.structure = s;
+    //   this.structures.push(s);
+    // }
+    // for (let i = 0; i < 5; i++) {
+    //   let pos = getRandomInt(0, this.vertices.length);
+    //   let c = getRandomInt(0, 4);
+    //   let s = new Structure(sCounter++, c, StructureType.Settlement);
+    //   this.vertices[pos]!.structure = s;
+    //   this.structures.push(s);
+    // }
+    // for (let i = 0; i < 5; i++) {
+    //   let pos = getRandomInt(0, this.vertices.length);
+    //   let c = getRandomInt(0, 4);
+    //   let s = new Structure(sCounter++, c, StructureType.Settlement);
+    //   this.vertices[pos]!.structure = s;
+    //   this.structures.push(s);
+    // }
+    const p1 = new Player(0, Colour.Blue);
+    const p2 = new Player(0, Colour.White);
+    const p3 = new Player(0, Colour.Orange);
+    const p4 = new Player(0, Colour.Red);
+    p1.cards.push(new Card(Resource.Brick));
+    p2.cards.push(new Card(Resource.Sheep));
+    p2.cards.push(new Card(Resource.Sheep));
+    p2.cards.push(new Card(Resource.Ore));
+    this.players.push(p1);
+    this.players.push(p2);
+    this.players.push(p3);
+    this.players.push(p4);
+
+  }
+}
